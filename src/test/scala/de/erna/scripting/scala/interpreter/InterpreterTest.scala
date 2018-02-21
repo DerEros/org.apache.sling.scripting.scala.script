57b4c80a3009287a1be615a623d9019b4cd92473
DerEros <DerEros@users.noreply.github.com>
2018-02-21 20:48:56 +0100

    More tests moved to ScalaTest

--- src/test/scala/de/erna/scripting/scala/interpreter/InterpreterTest.scala
+++ src/test/scala/de/erna/scripting/scala/interpreter/InterpreterTest.scala
@@ -16,21 +16,22 @@
  */
 package de.erna.scripting.scala.interpreter
 
-import java.io.File
-import junit.framework.TestCase
+import java.io.{File, PrintWriter}
+
 import de.erna.scripting.scala.Utils.valueOrElse
+import javax.script.ScriptException
+import junit.framework.Assert.{assertEquals, assertFalse}
+import org.scalatest.{BeforeAndAfter, FunSuite}
+
 import scala.tools.nsc.io.PlainFile
 
 /**
  * Standard test cases where files are read/written to/from the file system.
  */  
-class InterpreterTest extends TestCase with Tests {
-  
+class InterpreterTest extends FunSuite with BeforeAndAfter {
   var interpreterHelper: InterpreterHelper = null;
   
-  override def setUp() {
-    super.setUp();
-    
+  before {
     val workDir = new PlainFile(new File("target")).subdirectoryNamed("tmp")
     val srcDir = workDir.subdirectoryNamed("src")
     val outDir = workDir.subdirectoryNamed("classes")
@@ -41,10 +42,70 @@ class InterpreterTest extends TestCase with Tests {
       }
     }
   }
-  
-  override def tearDown() {
-    super.tearDown()
-    interpreterHelper = null
+
+  test ("Use eval to run a simple script") {
+    val code = "package a { class Testi(args: TestiArgs) { print(1 + 2) }}"
+    assertEquals("3", interpreterHelper.eval("a.Testi", code, Bindings()))
+  }
+
+  test ("Use eval to run failing script") {
+    val code = "syntax error"
+    try {
+      interpreterHelper.eval("a.Testi", code, Bindings())
+      fail("Expecting ScriptException")
+    }
+    catch {
+      case _: ScriptException =>  // expected
+    }
+  }
+
+  test ("Use eval to run script that throws error") {
+    val err = "Some error here";
+    val code = "package a { class Testi(args: TestiArgs) { throw new Error(\"" + err + "\") }}"
+    try {
+      interpreterHelper.eval("a.Testi", code, Bindings())
+      fail("Expecting Exception")
+    }
+    catch {
+      case e: ScriptException if err == e.getCause.getCause.getMessage => // expected
+    }
+  }
+
+  test ("Use eval to run script with bindings") {
+    val bindings = Bindings()
+    val time = java.util.Calendar.getInstance.getTime
+    bindings.putValue("msg", "Hello world")
+    bindings.putValue("time", time)
+    val code = "package a { class Testi(args: TestiArgs) {import args._; print(msg + \": \" + time)}}"
+    val result = interpreterHelper.eval("a.Testi", code, bindings)
+    assertEquals("Hello world: " + time, result)
+  }
+
+  test ("Use compile and execute to run script") {
+    val srcDir = interpreterHelper.srcDir
+    val interpreter = interpreterHelper.interpreter
+
+    val bindings = Bindings()
+    val time = java.util.Calendar.getInstance.getTime
+    bindings.putValue("msg", "Hello world")
+    bindings.putValue("time", time)
+
+    val code = "package a { class Testi(args: TestiArgs) {import args._; print(msg + \": \" + time)}}"
+    val src = srcDir.fileNamed("Testi.scala")
+    val writer = new PrintWriter(src.output)
+    writer.print(code)
+    writer.close
+
+    val out = new java.io.ByteArrayOutputStream
+    var result = interpreter.compile("a.Testi", src, bindings)
+    if (result.hasErrors) {
+      fail(result.toString)
+    }
+
+    result = interpreter.execute("a.Testi", bindings, None, Some(out))
+    assertFalse(result.hasErrors)
+
+    assertEquals("Hello world: " + time, out.toString)
   }
 
 }
